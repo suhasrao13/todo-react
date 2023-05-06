b672b63d5a6975abd10c06fe959d48c58c2f0eb4
9480309776 <83769454+9480309776@users.noreply.github.com>
2023-05-06 12:18:37 +0530

    add todo app with logic

---
+++ src/components/Todo.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const TodoApp = () => {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleInputChange = (event) => {
+    setTask(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (task.trim() !== "") {
+      setTasks([...tasks, task]);
+      setTask("");
+    }
+  };
+
+  const handleDelete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const handleComplete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = "✓ " + updatedTasks[index];
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={task}
+          onChange={handleInputChange}
+          placeholder="Add a task"
+        />
+        <button type="submit">Add Task</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span>{task}</span>
+            <button onClick={() => handleComplete(index)}>Complete</button>
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoApp;
